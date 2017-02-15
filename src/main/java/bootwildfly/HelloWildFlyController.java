ef2bb1b769b6a539e936db5457ebe56edab09253
huanbinzhang <huanbinzhang@hengtiansoft.com>
2017-02-15 16:59:08 +0800

    add mysql

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,14 +1,44 @@
 package bootwildfly;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWildFlyController {
 
+	@RequestMapping("hello")
+	public String sayHello() throws SQLException {
+		String name = "";
 
-    @RequestMapping("hello")
-    public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
-    }
+		Connection conn = null;
+		String sql;
+		String url = "jdbc:mysql://10.5.4.112:3306/hello?"
+				+ "user=root&password=123456&useUnicode=true&characterEncoding=UTF8";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			conn = DriverManager.getConnection(url);
+			Statement stmt = conn.createStatement();
+			sql = "select name from user";
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				name = rs.getString(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			conn.close();
+		}
+
+		return ("Hello " + name + ", SpringBoot on Wildfly");
+	}
 }
